c4c5287a5c00b8d97a3b2a0bfe842760750424e3
Frederik Wessel <frederikglimsager@hotmail.com>
2023-09-07 13:34:50 +0200

    Initial commit

---
+++ src/main/java/com/example/jpastudent/controller/StudentRestController.java
@@ -0,0 +1,40 @@
+package com.example.jpastudent.controller;
+
+import com.example.jpastudent.model.Student;
+import com.example.jpastudent.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class StudentRestController {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping("/students")
+    public List<Student> listStudents() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student postStudent(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student putStudent(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+
+}
